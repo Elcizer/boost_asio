b7ea8a08425b6c0e73f4e1d91105627936ff580c
Elcizer <114942856+Elcizer@users.noreply.github.com>
2025-09-15 14:51:30 +0000

    Client의 close 처리

--- Chatting/client.cpp
+++ Chatting/client.cpp
@@ -12,7 +12,7 @@ using boost::asio::ip::tcp;
 class tcp_connection{
     public:
         tcp_connection(boost::asio::io_context &ic,char *ip, char *port)
-            :resolver_(ic),socket_(ic)
+            :resolver_(ic),socket_(ic),isclose(false)
         {
             endpoint_ = resolver_.resolve(ip,port);
             boost::asio::connect(socket_,endpoint_);
@@ -31,11 +31,6 @@ class tcp_connection{
             socket_.write_some(boost::asio::buffer(name,32),ignored_error);
         }
         void start_read(){
-
-            // boost::asio::async_read(socket_,boost::asio::buffer(read_buf_),
-            //     boost::bind(&tcp_connection::read_handler,this,boost::asio::placeholders::error,
-            //     boost::asio::placeholders::bytes_transferred));
-
             boost::array<boost::asio::mutable_buffer, 2> buf_= 
                 {boost::asio::buffer(name_buf_),boost::asio::buffer(read_buf_)};
 
@@ -44,8 +39,14 @@ class tcp_connection{
                 boost::asio::placeholders::bytes_transferred));
         }
         void read_handler(const boost::system::error_code& ec,std::size_t bytes_transmitted){
-
-            // std::cout << read_buf_.data() <<"\n";
+            if(handle_ec(ec)){
+                if(!isclose){
+                    std::cout << "서버와의 연결이 종료되었습니다\n엔터를 누르면 프로그램이 종료됩니다.\n";
+                    socket_.close();
+                    isclose = true;
+                }
+                return;
+            }
             std::cout << name_buf_.data()<<" : " << read_buf_.data() <<"\n";
             start_read();
         }
@@ -56,11 +57,36 @@ class tcp_connection{
                 boost::asio::placeholders::error,boost::asio::placeholders::bytes_transferred));
         }
         void write_handler(const boost::system::error_code& ec,std::size_t bytes_transmitted){
+            if(handle_ec(ec)){
+                if(!isclose){
+                    socket_.close();
+                    isclose = true;
+                }
+                return;
+            }
             start_write();
+        }
+        bool handle_ec(const boost::system::error_code& ec){ // 오류 -> false 
+            if(ec == boost::asio::error::eof){ // eof 전송 보통 close() 이용 
+                return true;
+            }
+            else if(ec == boost::asio::error::connection_reset){
+                return true;
+            }
+            else if(ec == boost::asio::error::connection_aborted){
+                return true;
+            }
+            else if(ec == boost::asio::error::operation_aborted){
+                return true;
+            }
+            else if(ec == boost::asio::error::bad_descriptor){
+                return true;
+            }
+            return false;
         }
 
 
-
+        bool isclose;
         tcp::resolver::results_type endpoint_;
         tcp::resolver resolver_;
         tcp::socket socket_;
